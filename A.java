ec9ab21b94407aeac244d35e81203d27fa025cc0
oyh <sksms45120@gmail.com>
2022-07-06 19:03:05 +0900

    test1

--- A.java
+++ A.java
@@ -4,5 +4,9 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
+		System.out.println("git test");
 	}
 }
